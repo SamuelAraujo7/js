ca713b346ae37539364d33ca3894a323d20fa321
Samuel Araújo <91102877+SamuelAraujo7@users.noreply.github.com>
2022-12-19 16:10:42 -0300

    aula 12 js

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 67
+if(idade < 16){
+    console.log(`Não vota`)
+}else if( idade >= 16 && idade < 18 || idade >65){
+    console.log(`Vota se quiser`)
+    
+}else{
+    console.log(`Obrigado a votar`)
+}
