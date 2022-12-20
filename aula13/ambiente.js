be2ba9d84585db2294c8111301c727fb4fc394c2
Samuel Araújo <91102877+SamuelAraujo7@users.noreply.github.com>
2022-12-20 10:49:25 -0300

    aula 13 e 14 estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,33 @@
+/*var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 5)
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
+
